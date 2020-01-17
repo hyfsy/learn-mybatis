78c0a9ede084a7eebf17072a5bf1c457620b5595
hyfsy <1577975140@qq.com>
2020-01-17 14:35:46 +0800

    简单了解一级缓存及二级缓存，使用ehcache缓存

--- src/main/java/com/hyf/mybatis/test/TestCache.java
+++ src/main/java/com/hyf/mybatis/test/TestCache.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
 
 public class TestCache {
 
+    /**
+     * 测试一级缓存
+     */
     @Test
     public void testOneCache () {
         try (SqlSession sqlSession = MyBatisUtil.getSqlSession()) {
@@ -22,4 +25,32 @@ public class TestCache {
             System.out.println(user);
         }
     }
+
+    /**
+     * 测试二级缓存 + ehcache缓存
+     */
+    @Test
+    public void testTwoCache () {
+        try (SqlSession sqlSession = MyBatisUtil.getSqlSession();
+             SqlSession sqlSession2 = MyBatisUtil.getSqlSession()) {
+            UserCacheMapper mapper = sqlSession.getMapper(UserCacheMapper.class);
+            UserCache user = mapper.testOneCache(1);
+            System.out.println(user);
+
+            sqlSession.close();
+            mapper = sqlSession2.getMapper(UserCacheMapper.class);
+
+            // 日志输出缓存命中率
+            user = mapper.testOneCache(1);
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testEhCacheDefaultDir () {
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("user.home"));
+        System.out.println(System.getProperty("java.io.tmpdir"));
+    }
+
 }
